c44717783a25c4f10b0cb203b4a1ba2e968935c1
Bob Pascazio <bob@bytefly.com>
2015-12-15 19:13:21 -0500

    Updates

--- FontAwesomeTest/ViewController.swift
+++ FontAwesomeTest/ViewController.swift
@@ -7,12 +7,26 @@
 //
 
 import UIKit
+import FontAwesome_swift
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var button: UIButton!
+    @IBOutlet weak var label: UILabel!
+    @IBOutlet weak var leftBarButton: UIBarButtonItem!
+    @IBOutlet weak var rightBarButton: UIBarButtonItem!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        label.font = UIFont.fontAwesomeOfSize(100)
+        label.text = String.fontAwesomeIconWithName(FontAwesome.Github)
+        
+        button.titleLabel?.font = UIFont.fontAwesomeOfSize(30)
+        button.setTitle(String.fontAwesomeIconWithName(.Laptop), forState: .Normal)
+        
+        let attributes = [NSFontAttributeName: UIFont.fontAwesomeOfSize(20)] as Dictionary!
+        leftBarButton.setTitleTextAttributes(attributes, forState: .Normal)
+        leftBarButton.title = String.fontAwesomeIconWithName(.Rocket)
     }
 
     override func didReceiveMemoryWarning() {
